42ddd6c16f310173110630939ed7b7492beed450
ziyoxon <mirziyoxonm@gmail.com>
2024-10-28 08:43:13 +0500

    cicd action

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+
+describe('AppController', () => {
+  let appController: AppController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true })],
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe('root', () => {
+    it('should return "Hello World! PORT = 3000"', () => {
+      expect(appController.getHello()).toBe('Hello World! PORT = 3000');
+    });
+  });
+});
